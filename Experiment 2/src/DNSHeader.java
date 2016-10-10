d955518eff562a0f99d0e1d05950e2144dc5c561
Ravi Chaganti <ravi.chaganti@mail.mcgill.ca>
2016-10-10 02:04:48 -0400

    updated DNS header for 12 bytes size

--- Experiment 2/src/DNSHeader.java
+++ Experiment 2/src/DNSHeader.java
@@ -1,40 +1,67 @@
+import java.nio.*;
+import java.util.*;
+
 /*
- * This file includes the format of parameters used to define a DNS Header
+ * This file includes the format of parameters to define a DNS Header
  */
 
 public class DNSHeader {
 	
-	public byte QR;				//Query/Response Flag
-	public byte Opcode;			//Operation Code
-	public byte AA;				//Authoritative Answer Flag
-	public byte TC;				//Truncation Flag
-	public byte RD;				//Recursion Desired
-	public byte RA;				//Recursion Available
-	public byte Z;				//Zero
-	public byte RCode;			//RCode
+	public static final int sizeOfHeader = 12;
+	private ByteBuffer HeaderNode = ByteBuffer.allocate(sizeOfHeader);
 	
-	public short ID;			//Identifier
-	public short QDCount;		//Question count
-	public short ANCount;		//Answer record count
-	public short NSCount;		//Authority record count
-	public short ARCount;		//Additional record count
+	private byte QR;				//Query/Response Flag
+	private byte Opcode;			//Operation Code
+	private byte AA;				//Authoritative Answer Flag
+	private byte TC;				//Truncation Flag
+	private byte RD;				//Recursion Desired
+	private byte RA;				//Recursion Available
+	private byte Z;				//Zero
+	private byte RCode;			//RCode
+	
+	private short ID;			//Identifier
+	private short QDCount;		//Question count
+	private short ANCount;		//Answer record count
+	private short NSCount;		//Authority record count
+	private short ARCount;		//Additional record count
 	
 	
-	public DNSHeader(byte QR, byte Opcode, byte AA, byte TC, byte RD, byte RA, byte Z, byte RCode, short ID, short QDCount, short ANCount, short NSCount, short ARCount) {
-		this.QR = QR;
-		this.Opcode = Opcode;
-		this.AA = AA;
-		this.TC = TC;
-		this.RD = RD;
-		this.RA = RA;
-		this.Z = Z;
-		this.RCode = RCode;
-		this.ID = ID;
-		this.QDCount = QDCount;
-		this.ANCount = ANCount;
-		this.NSCount = NSCount;
-		this.ARCount = ARCount;
+	public DNSHeader () {
+		
+		QR = (byte) 0;
+		Opcode = (byte) 0;
+		AA = (byte) 0;
+		TC = (byte) 0;
+		RD = (byte) 1;
+		RA = (byte) 0;
+		Z = (byte) 0;
+		RCode = (byte) 0;
+		
+		Random random = new Random();
+		ID = (short) random.nextInt(Short.MAX_VALUE + 1);
+		QDCount = (short) 1;
+		ANCount = (short) 0;
+		NSCount = (short) 0;
+		ARCount = (short) 0;
+		
+		HeaderNode.putShort(ID);
+		
+		//convert 2nd layer of DNS Header into 16 bit type (short)
+		short conversion = (short) (QR << 15 | Opcode << 11 | AA << 10 | TC << 9 | 
+									RD << 8 | RA << 7 | Z << 4 | RCode);
+		
+		HeaderNode.putShort(conversion);
+		HeaderNode.putShort(QDCount);
+		HeaderNode.putShort(ANCount);
+		HeaderNode.putShort(NSCount);
+		HeaderNode.putShort(ARCount);
 	}
+
+	
+	public ByteBuffer GetHeaderNode() {
+		return HeaderNode;
+	}
+
 	
 	public byte GetQR() {
 		return QR;
